4f89737b62f9fefd1df31f1e04f9b6ff0ecfab4a
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-03-07 15:01:06 +0100

    nrf: Add interrupt support to microcontroller and add remaining pins.

--- nrf5/common-hal/microcontroller/__init__.c
+++ nrf5/common-hal/microcontroller/__init__.c
@@ -33,29 +33,71 @@ void common_hal_mcu_delay_us(uint32_t delay) {
 }
 
 void common_hal_mcu_disable_interrupts(void) {
-    // TODO(tannewt)
+#ifndef BLUETOOTH_SD
+    __disable_irq();
+#endif
 }
 
 void common_hal_mcu_enable_interrupts(void) {
-    // TODO(tannewt)
+#ifndef BLUETOOTH_SD
+    __enable_irq();
+#endif
 }
 
 // This maps MCU pin names to pin objects.
 STATIC const mp_map_elem_t mcu_pin_global_dict_table[] = {
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_00), (mp_obj_t)&pin_P0_00 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_01), (mp_obj_t)&pin_P0_01 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_02), (mp_obj_t)&pin_P0_02 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_03), (mp_obj_t)&pin_P0_03 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_13), (mp_obj_t)&pin_P0_13 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_14), (mp_obj_t)&pin_P0_14 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_15), (mp_obj_t)&pin_P0_15 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_16), (mp_obj_t)&pin_P0_16 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_17), (mp_obj_t)&pin_P0_17 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_18), (mp_obj_t)&pin_P0_18 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_19), (mp_obj_t)&pin_P0_19 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_20), (mp_obj_t)&pin_P0_20 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_21), (mp_obj_t)&pin_P0_21 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_22), (mp_obj_t)&pin_P0_22 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_P0_23), (mp_obj_t)&pin_P0_23 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_00), (mp_obj_t)&pin_P0_00 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_01), (mp_obj_t)&pin_P0_01 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_02), (mp_obj_t)&pin_P0_02 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_03), (mp_obj_t)&pin_P0_03 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_04), (mp_obj_t)&pin_P0_04 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_05), (mp_obj_t)&pin_P0_05 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_06), (mp_obj_t)&pin_P0_06 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_07), (mp_obj_t)&pin_P0_07 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_08), (mp_obj_t)&pin_P0_08 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_09), (mp_obj_t)&pin_P0_09 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_10), (mp_obj_t)&pin_P0_10 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_11), (mp_obj_t)&pin_P0_11 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_12), (mp_obj_t)&pin_P0_12 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_13), (mp_obj_t)&pin_P0_13 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_14), (mp_obj_t)&pin_P0_14 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_15), (mp_obj_t)&pin_P0_15 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_16), (mp_obj_t)&pin_P0_16 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_17), (mp_obj_t)&pin_P0_17 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_18), (mp_obj_t)&pin_P0_18 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_19), (mp_obj_t)&pin_P0_19 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_20), (mp_obj_t)&pin_P0_20 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_21), (mp_obj_t)&pin_P0_21 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_22), (mp_obj_t)&pin_P0_22 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_23), (mp_obj_t)&pin_P0_23 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_24), (mp_obj_t)&pin_P0_24 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_25), (mp_obj_t)&pin_P0_25 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_26), (mp_obj_t)&pin_P0_26 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_27), (mp_obj_t)&pin_P0_27 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_28), (mp_obj_t)&pin_P0_28 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_29), (mp_obj_t)&pin_P0_29 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_30), (mp_obj_t)&pin_P0_30 },
+    #if defined(NRF52832_XXAA) || defined(NRF52840_XXAA)
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P0_31), (mp_obj_t)&pin_P0_31 },
+    #endif
+
+    #ifdef NRF52840_XXAA
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_00), (mp_obj_t)&pin_P1_00 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_01), (mp_obj_t)&pin_P1_01 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_02), (mp_obj_t)&pin_P1_02 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_03), (mp_obj_t)&pin_P1_03 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_04), (mp_obj_t)&pin_P1_04 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_05), (mp_obj_t)&pin_P1_05 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_06), (mp_obj_t)&pin_P1_06 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_07), (mp_obj_t)&pin_P1_07 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_08), (mp_obj_t)&pin_P1_08 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_09), (mp_obj_t)&pin_P1_09 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_10), (mp_obj_t)&pin_P1_10 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_11), (mp_obj_t)&pin_P1_11 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_12), (mp_obj_t)&pin_P1_12 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_13), (mp_obj_t)&pin_P1_13 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_14), (mp_obj_t)&pin_P1_14 },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_P1_15), (mp_obj_t)&pin_P1_15 },
+    #endif
 };
 MP_DEFINE_CONST_DICT(mcu_pin_globals, mcu_pin_global_dict_table);
