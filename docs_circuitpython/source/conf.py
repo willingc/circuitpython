6ecf8886bdcfe5f09d7bdf6a0544030bf4067e6b
Carol Willing <carolcode@willingconsulting.com>
2017-09-07 04:29:55 -0700

    remote help.rst

--- docs_circuitpython/source/conf.py
+++ docs_circuitpython/source/conf.py
@@ -121,7 +121,7 @@ exclude_patterns = [
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-#default_role = 'any'
+default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
