0450cfa95d7e030ae16d511c73fa5225ebe588ee
Carol Willing <carolcode@willingconsulting.com>
2017-09-08 19:54:05 -0700

    Comment c2rst module

--- docs_circuitpython/source/c2rst.py
+++ docs_circuitpython/source/c2rst.py
@@ -1,17 +1,28 @@
+"""Helper to parse C comments to RestructuredText"""
 import docutils.parsers
 import docutils.parsers.rst as rst
 
 class CStrip(docutils.parsers.Parser):
-  def __init__(self):
-    self.rst_parser = rst.Parser()
+    """Strip comment prefixes from C comments
 
-  def parse(self, inputstring, document):
-    stripped = []
-    for line in inputstring.split("\n"):
-      line = line.strip()
-      if line == "//|":
-        stripped.append("")
-      elif line.startswith("//| "):
-        stripped.append(line[len("//| "):])
-    stripped = "\r\n".join(stripped)
-    self.rst_parser.parse(stripped, document)
+    TODO: determine if sphinx parser could work
+
+    """
+    def __init__(self):
+        self.rst_parser = rst.Parser()
+
+    def parse(self, inputstring, document):
+        """Strip comment indicators from a string and place in a document."""
+        stripped = []
+
+        # iterate through individual lines of text
+        for line in inputstring.split('\n'):
+            # strip leading and trailing characters
+            line = line.strip()
+            if line == '//|':
+                stripped.append('')
+            elif line.startswith('//| '):
+                stripped.append(line[len("//| "):])
+
+        stripped = "\r\n".join(stripped)
+        self.rst_parser.parse(stripped, document)
